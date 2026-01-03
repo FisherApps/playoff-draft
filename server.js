9c39c3d4bf796b4864ae217b0fd0bcec2515d3ca
Brian Fisher <brianfisher@mac.mynetworksettings.com>
2026-01-02 21:54:05 -0500

    Fix: bind to 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -625,7 +625,6 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Share via ngrok: ngrok http ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
